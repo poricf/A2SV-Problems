ac209132928646624afa4cdae3d6cff3b47b8284
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-01-15 21:31:38 +0300

    Added solution for Delete Columns to Make Sorted

---
+++ phase-1/problems/delete-columns-to-make-sorted.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        count=0
+        
+        for i in zip(*strs):
+            if(sorted(i)!=list(i)):
+                count+=1
+                
+                
+        return count
