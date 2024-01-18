d2bde6dda7254bd59777f3aa73621623eca8187b
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-01-18 12:56:28 +0300

    Added solution for Frequency of the Most Frequent Element

---
+++ phase-1/problems/frequency-of-the-most-frequent-element.cpp
@@ -0,0 +1,21 @@
+#define a(x) x.begin(),x.end()
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        int n=nums.size();
+        int a=0, b=0;
+        sort(a(nums));
+        vector<long long> prefix(n,nums[0]);
+        for(int i=1; i<n; i++)
+            prefix[i]=nums[i]+prefix[i-1];
+        int ans=0;
+        while(b<n){
+            while((nums[b]*(b-a+1LL)) - (prefix[b]-(a?prefix[a-1]:0LL)) > k){
+                a++;
+            }
+            ans= max(ans, b-a+1);
+            b++;
+        }
+        return ans;
+    }
+};
