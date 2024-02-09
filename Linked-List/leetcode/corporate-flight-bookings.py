b2e180b087648cd6d41ea28e43f81492d0b198dc
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-02-09 20:32:58 +0300

    Add solution for Corporate Flight Bookings

---
+++ Linked-List/leetcode/corporate-flight-bookings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
+
+        arr = [0]*(n+1)
+        for lv, ar, seats in bookings:
+            arr[lv-1]+= seats
+            arr[ar]-= seats
+
+        return list(accumulate(arr[:-1]))
