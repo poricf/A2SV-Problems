1949ad96107c401557b9ba98eb93180cd3bd766b
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-02-22 10:57:57 +0300

    Add solution for Implement Stack using Queues

---
+++ Linked-List/leetcode/implement-stack-using-queues.py
@@ -0,0 +1,15 @@
+class MyStack:
+    def __init__(self):
+        
+        self.stack = []
+    def push(self, x):
+        self.stack.append(x)
+    def pop(self):
+        return self.stack.pop()
+    def top(self):
+        
+        return self.stack[-1]
+
+    def empty(self):
+        
+        return len(self.stack) == 0
