c6c419f817bfd10e5ee736c467a95d091ca7dfd5
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-02-28 22:08:44 +0300

    Add solution for Intersection of Two Arrays II

---
+++ Progress-Sheet/0x03-Sorting/leetcode/intersection-of-two-arrays-ii.py
@@ -0,0 +1,11 @@
+from collections import Counter
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        co = Counter(nums1)
+        co2 = Counter(nums2)
+        k = set(nums1)&set(nums2)
+        res = []
+        for num in list(k):
+            res.extend([num]*min(co[num],co2[num]))
+        return res
+        
