58314006e26c68ab01cb6aade0c5d71cf04144e1
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-03-06 02:28:54 +0300

    Add solution for Permutations

--- Progress-Sheet/0x09b-Backtracking/leetcode/permutations.py
+++ Progress-Sheet/0x09b-Backtracking/leetcode/permutations.py
@@ -1,3 +1,24 @@
+'''
+Apprach 1 swapping
+'''
+class Solution:
+    ans = []
+    def recurPermute(self, index: int, nums: List[int], ans: List[List[int]]):
+        if index == len(nums):
+            ans.append(nums[:])
+            return
+        for i in range(index, len(nums)):
+            nums[index], nums[i] = nums[i], nums[index]
+            self.recurPermute(index + 1, nums, ans)
+            nums[index], nums[i] = nums[i], nums[index]
+
+
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        self.ans = []
+        self.recurPermute(0, nums, self.ans)
+        return self.ans
+'''
+Pproach 2 using map
 class Solution:
     
     ans = []
@@ -26,4 +47,4 @@ class Solution:
 
 
         
-
+'''
