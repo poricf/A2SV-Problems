e0cdbde11bda910af6a84694e5d47288139d29ed
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-03-06 03:48:48 +0300

    Delete Progress-Sheet/0x06-LinkedList/leetcode/separate-black-and-white-balls.py

--- Progress-Sheet/0x06-LinkedList/leetcode/separate-black-and-white-balls.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def minimumSteps(self, s: str) -> int:
-        count , ans = 0 , 0
-        for i in range(len(s)-1 ,-1 , -1):
-            if s[i] == '0':
-                count += 1 
-            else:
-                ans += count
-        return ans 
