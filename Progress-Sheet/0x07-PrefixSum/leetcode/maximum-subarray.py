0dfd0f78c01c6dff485bbd354be82c9cebde35f2
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-03-06 14:40:12 +0300

    Add solution for Maximum Subarray

---
+++ Progress-Sheet/0x07-PrefixSum/leetcode/maximum-subarray.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        s, maxx = 0, nums[0]
+        for i in nums:
+            s += i
+            maxx = max(s,maxx)
+            if s<0:
+                s = 0
+        return maxx 
