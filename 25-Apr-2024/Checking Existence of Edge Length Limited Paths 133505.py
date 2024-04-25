eb39836281a6244912e70b51139a0f4713d6ffb6
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-04-25 19:21:54 +0300

    Created 'Checking Existence of Edge Length Limited Paths' by A2SV Hub

---
+++ 25-Apr-2024/Checking Existence of Edge Length Limited Paths 133505.py
@@ -0,0 +1,71 @@
+# Problem: Checking Existence of Edge Length Limited Paths - https://leetcode.com/problems/checking-existence-of-edge-length-limited-paths/
+
+class DSU:
+    def __init__(self, n):
+        self.rank = [0] * (n + 1)
+        self.parent = list(range(n + 1))
+        self.size = [1] * (n + 1)
+
+    def find(self, node):
+        if node == self.parent[node]:
+            return node
+
+        stack = []
+        while node != self.parent[node]:
+            stack.append(node)
+            node = self.parent[node]
+
+        # Path compression
+        for n in stack:
+            self.parent[n] = node
+
+        return node
+
+    def Runion(self, u, v):
+        rep_u = self.find(u)
+        rep_v = self.find(v)
+        if rep_u == rep_v:
+            return
+        if self.rank[rep_u] < self.rank[rep_v]:
+            self.parent[rep_u] = rep_v
+        elif self.rank[rep_v] < self.rank[rep_u]:
+            self.parent[rep_v] = rep_u
+        else:
+            self.parent[rep_v] = rep_u
+            self.rank[rep_u] += 1
+
+    def Sunion(self, u, v):
+        rep_u = self.find(u)
+        rep_v = self.find(v)
+        if rep_u == rep_v:
+            return
+        if self.size[rep_u] < self.size[rep_v]:
+            self.parent[rep_u] = rep_v
+            self.size[rep_v] += self.size[rep_u]
+        else:
+            self.parent[rep_v] = rep_u
+            self.size[rep_u] += self.size[rep_v]
+
+
+class Solution:
+    def distanceLimitedPathsExist(self, n: int, edgeList: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        ans = [False] * len(queries)
+        dsu = DSU(n)
+
+        for i in range(len(queries)):
+            queries[i].append(i)
+
+        edgeList.sort(key=lambda x: x[2])
+        queries.sort(key=lambda x: x[2])
+
+        i = 0  
+        for query in queries:
+            while i < len(edgeList) and edgeList[i][2] < query[2]:
+                u, v = edgeList[i][0], edgeList[i][1]
+                dsu.Runion(u, v)
+                i += 1
+            if dsu.find(query[0]) == dsu.find(query[1]):
+                ans[query[3]] = True
+
+        return ans
+        
