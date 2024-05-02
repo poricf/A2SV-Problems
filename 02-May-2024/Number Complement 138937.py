140b39c2f43f790e654940f6f880517955d51926
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-05-02 09:07:22 +0300

    Created 'Number Complement' by A2SV Hub

---
+++ 02-May-2024/Number Complement 138937.py
@@ -0,0 +1,7 @@
+# Problem: Number Complement - https://leetcode.com/problems/number-complement/
+
+class Solution:
+    def findComplement(self, num: int) -> int:
+        k = num.bit_length()
+        ans = num ^ (2**k - 1)
+        return ans
