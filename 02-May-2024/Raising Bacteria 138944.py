d7cab72c9069d775f9fd057c43a7aca460284167
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-05-02 09:20:55 +0300

    Created 'Raising Bacteria' by A2SV Hub

---
+++ 02-May-2024/Raising Bacteria 138944.py
@@ -0,0 +1,36 @@
+# Problem: Raising Bacteria - https://codeforces.com/contest/579/problem/A
+
+######################################################################
+###########     Author - Fahmi Dinsefa                  ##############
+###########     Created at 4/8/2024                     ############## 
+######################################################################
+
+import sys
+sys.setrecursionlimit(10**3)
+INF = float('inf')
+N = 10**5 + 10
+alpha = '/abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ/' 
+
+def I(): return int(sys.stdin.readline().strip())
+def ILT(): return list(map(int, sys.stdin.readline().strip().split()))
+def S(): return sys.stdin.readline().strip()
+def SLT(): return sys.stdin.readline().strip().split()
+def DIG(): return [int(i) for i in (list(sys.stdin.readline().strip()))]
+def CHAR(): return list(sys.stdin.readline().strip())
+ORD = lambda: [ord(char) for char in sys.stdin.readline().strip()]
+
+
+from collections import Counter, defaultdict, deque
+from bisect import bisect_right, bisect_left
+from math import ceil, sqrt, gcd
+from heapq import heapify, heappop, heapreplace, heappush
+
+
+
+
+def solve():
+    n = I()
+    print(n.bit_count())
+
+# for _ in range(I()):
+solve()
