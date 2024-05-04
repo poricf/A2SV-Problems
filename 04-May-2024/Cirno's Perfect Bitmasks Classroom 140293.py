698ee9aadc41e2daeb678c1728123f135d1e5e19
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-05-04 18:26:50 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 04-May-2024/Cirno's Perfect Bitmasks Classroom 140293.py
@@ -0,0 +1,44 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+######################################################################
+###########     Author - Fahmi Dinsefa                  ##############
+###########     Created at 5/10/2024                     ############## 
+######################################################################
+
+import sys
+sys.setrecursionlimit(10**3)
+INF = float('inf')
+N = 10**5 + 10
+alpha = '/abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ/' 
+
+def I(): return int(sys.stdin.readline().strip())
+def ILT(): return list(map(int, sys.stdin.readline().strip().split()))
+def S(): return sys.stdin.readline().strip()
+def SLT(): return sys.stdin.readline().strip().split()
+def DIG(): return [int(i) for i in (list(sys.stdin.readline().strip()))]
+def CHAR(): return list(sys.stdin.readline().strip())
+ORD = lambda: [ord(char) for char in sys.stdin.readline().strip()]
+
+
+from collections import Counter, defaultdict, deque
+from bisect import bisect_right, bisect_left
+from math import ceil, sqrt, gcd
+from heapq import heapify, heappop, heapreplace, heappush
+
+
+
+
+def solve():
+
+    n = int(input())
+    if n == 1:
+        print(3)
+        return
+    bans = n & -n
+    if n != bans:
+        print(bans)
+    else:
+        print(n + 1)
+
+for _ in range(I()):
+    solve()
