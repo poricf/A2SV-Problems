314af5c40a1ed39135b6e05d7b213a4a55325d61
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-05-19 18:23:47 +0300

    Created 'Longest Nice Substring' by A2SV Hub

---
+++ 19-May-2024/Longest Nice Substring 154798.py
@@ -0,0 +1,10 @@
+# Problem: Longest Nice Substring - https://leetcode.com/problems/longest-nice-substring/
+
+class Solution:
+    def longestNiceSubstring(self, s: str) -> str:
+        ans = ""
+        for i in range(len(s)):
+            for ii in range(i+1, len(s)+1):
+                if all(s[k].swapcase() in s[i:ii] for k in range(i, ii)): 
+                    ans = max(ans, s[i:ii], key=len)
+        return ans 
