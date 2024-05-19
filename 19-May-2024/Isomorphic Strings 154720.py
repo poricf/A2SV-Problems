dead0c0541d6798e117c11d059f9fcc29782e9fe
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-05-19 14:34:41 +0300

    Created 'Isomorphic Strings' by A2SV Hub

---
+++ 19-May-2024/Isomorphic Strings 154720.py
@@ -0,0 +1,23 @@
+# Problem: Isomorphic Strings - https://leetcode.com/problems/isomorphic-strings/
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        dict = {}
+        hSet = set()
+
+        for i in range (len(s)):
+            sChar = s[i]
+            tChar = t[i]
+            if (sChar not in dict):
+                if(tChar in hSet):
+                    return False
+                
+                dict[sChar] = tChar
+                hSet.add(tChar)
+            
+            else:    #check if exists
+                if(tChar != dict[sChar]):
+                    return False
+
+
+        return True
