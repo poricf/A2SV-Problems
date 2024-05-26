207fd0ed7408ed97b0cd23befeb9c9854c881048
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-05-26 21:28:13 +0300

    Created 'Longest Word in Dictionary' by A2SV Hub

---
+++ 26-May-2024/Longest Word in Dictionary 159807.py
@@ -0,0 +1,15 @@
+# Problem: Longest Word in Dictionary - https://leetcode.com/problems/longest-word-in-dictionary/
+
+class Solution:
+    def longestWord(self, words: List[str]) -> str:
+        words.sort()
+        visited = set([''])
+        ans = ''
+        for i in range(len(words)):
+            wrd = words[i][:-1]
+            word = words[i]
+            if wrd in visited:
+                if len(word) > len(ans) or (len(word) == len(ans) and word < ans):
+                    ans = word 
+                visited.add(word)
+        return ans
