a8317cabf42cdbe9ea60405bf25951634ba75a18
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-06-16 22:07:25 +0300

    Created 'A - Spris' by A2SV Hub

---
+++ A2SV G5 - Contest #20 16-Jun-2024/A - Spris 168960.py
@@ -0,0 +1,48 @@
+# Problem: A - Spris - https://codeforces.com/gym/528792/problem/A
+
+######################################################################
+###########     Author - Fahmi Dinsefa                  ##############
+###########     Created at 4/8/2024                     ############## 
+######################################################################
+
+import sys
+sys.setrecursionlimit(10**3)
+INF = float('inf')
+N = 10**5 + 10
+alpha = '/abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ/' 
+
+def I(): return int(sys.stdin.readline().strip())
+def ILT(): return list(map(int, sys.stdin.readline().strip().split()))
+def S(): return sys.stdin.readline().strip()
+def SLT(): return sys.stdin.readline().strip().split()
+def DIG(): return [int(i) for i in (list(sys.stdin.readline().strip()))]
+def CHAR(): return list(sys.stdin.readline().strip())
+ORD = lambda: [ord(char) for char in sys.stdin.readline().strip()]
+
+
+from collections import Counter, defaultdict, deque
+from bisect import bisect_right, bisect_left
+from math import ceil, sqrt, gcd
+from heapq import heapify, heappop, heapreplace, heappush
+
+
+
+
+def solve():
+    a = I()
+    b = I()
+    c = I()
+    
+    val1 = c//4
+    val2 = b//2
+    aval = min(val1,val2)
+    if a < aval: 
+        aval = a
+    
+    bval = aval*2
+    cval = aval*4
+
+    print(aval*7)
+
+# for _ in range(I()):
+solve()
