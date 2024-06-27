fc947bf26d36cc5ae3d301922db319e4eb4908bf
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-06-27 22:28:24 +0300

    Created 'Symmetric tree' by A2SV Hub

---
+++ 27-Jun-2024/Symmetric tree 173453.py
@@ -0,0 +1,21 @@
+# Problem: Symmetric tree - https://leetcode.com/problems/symmetric-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def Symmetric(a,b):
+            if type(a) == TreeNode and type(b) == TreeNode:
+                if a.val == b.val:
+                    one = Symmetric(a.left,b.right)
+                    two = Symmetric(a.right,b.left)
+                    return one and two
+            else:
+                return a==b
+
+        
+        return Symmetric(root.left,root.right)
