961450926a6c19b39a36bb2f1c239054d9c6d94a
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-08-10 14:50:59 +0300

    Created 'Reach a Number' by A2SV Hub

---
+++ 10-Aug-2024/Reach a Number 190301.py
@@ -0,0 +1,13 @@
+# Problem: Reach a Number - https://leetcode.com/problems/reach-a-number/
+
+class Solution:
+    def reachNumber(self, target: int) -> int:
+        ans, k = 0, 0
+        target = abs(target)
+        while ans < target:
+            ans += k
+            k += 1
+        while (ans - target) % 2:
+            ans += k
+            k += 1
+        return k - 1  
