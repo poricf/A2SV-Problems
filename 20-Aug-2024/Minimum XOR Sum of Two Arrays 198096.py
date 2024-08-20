915665e440d853e2faf10117c147699c4a7e0ca1
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-08-20 11:10:34 +0300

    Created 'Minimum XOR Sum of Two Arrays' by A2SV Hub

---
+++ 20-Aug-2024/Minimum XOR Sum of Two Arrays 198096.py
@@ -0,0 +1,13 @@
+# Problem: Minimum XOR Sum of Two Arrays - https://leetcode.com/problems/minimum-xor-sum-of-two-arrays/
+
+class Solution:
+    def minimumXORSum(self, a: List[int], b: List[int]) -> int:
+        @cache
+        def dp(mask: int) -> int:
+            i = bin(mask).count("1")
+            if i >= len(a):
+                return 0
+            return min((a[i] ^ b[j]) + dp(mask + (1 << j)) 
+                       for j in range(len(b)) if mask & (1 << j) == 0)
+        
+        return dp(0)
