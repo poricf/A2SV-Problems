2ae238ce97835559deba0d05cc3505c248ab87c9
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-08-21 16:47:40 +0300

    Created 'Maximum XOR for Each Query' by A2SV Hub

---
+++ 21-Aug-2024/Maximum XOR for Each Query 198400.py
@@ -0,0 +1,26 @@
+# Problem: Maximum XOR for Each Query - https://leetcode.com/problems/maximum-xor-for-each-query/
+
+class Solution:
+    def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]: 
+        n = len(nums)
+        def toggle(x, i,z):
+            for i in range(z):
+                x = ((1 << i) ^ x)
+            return x
+        ans = 0
+        result = []
+        for num in nums:
+            ans ^= num
+        print(ans)
+        
+
+
+        
+        for i in range(n-1,-1,-1):
+            
+            init = toggle(ans,i,maximumBit)
+            result.append(init)
+            ans ^= nums[i]
+        
+
+        return result
