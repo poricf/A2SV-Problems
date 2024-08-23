3ebd150cc74ce15d2be4df92da5e633fcbafeea8
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-08-23 07:53:23 +0300

    Created 'Count Collisions of Monkeys on a Polygon' by A2SV Hub

---
+++ 23-Aug-2024/Count Collisions of Monkeys on a Polygon 198883.py
@@ -0,0 +1,8 @@
+# Problem: Count Collisions of Monkeys on a Polygon - https://leetcode.com/problems/count-collisions-of-monkeys-on-a-polygon/
+
+class Solution:
+    def monkeyMove(self, n: int) -> int:
+        mod = 10 ** 9 + 7
+        res = pow(2,n,mod)
+
+        return (res - 2) % mod
