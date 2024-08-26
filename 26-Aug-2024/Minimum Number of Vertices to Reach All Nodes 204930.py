67813088d9360aaf49cee37ef5a23746967c5bb8
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-08-26 22:27:37 +0300

    Created 'Minimum Number of Vertices to Reach All Nodes' by A2SV Hub

---
+++ 26-Aug-2024/Minimum Number of Vertices to Reach All Nodes 204930.py
@@ -0,0 +1,14 @@
+# Problem: Minimum Number of Vertices to Reach All Nodes - https://leetcode.com/problems/minimum-number-of-vertices-to-reach-all-nodes/
+
+class Solution:
+    def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
+        parent=[False]*n
+        for u,v in edges:
+            parent[v]=True
+
+        ans=[]
+        for u in range(n):
+            if not parent[u]:
+                ans.append(u)
+
+        return ans            
