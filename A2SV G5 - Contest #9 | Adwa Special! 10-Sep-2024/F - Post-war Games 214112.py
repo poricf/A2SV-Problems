80777bed6a4f5496aeb7470ece5560e24ccf48a3
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-09-10 18:02:29 +0300

    Created 'F - Post-war Games' by A2SV Hub

---
+++ A2SV G5 - Contest #9 | Adwa Special! 10-Sep-2024/F - Post-war Games 214112.py
@@ -0,0 +1,36 @@
+# Problem: F - Post-war Games - https://codeforces.com/gym/513152/problem/F
+
+
+import sys
+sys.setrecursionlimit(10**3)
+INF = float('inf')
+N = 10**5 + 10
+alpha = '/abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ/' 
+
+def I(): return int(sys.stdin.readline().strip())
+def ILT(): return list(map(int, sys.stdin.readline().strip().split()))
+def S(): return sys.stdin.readline().strip()
+def SLT(): return sys.stdin.readline().strip().split()
+def DIG(): return [int(i) for i in (list(sys.stdin.readline().strip()))]
+def CHAR(): return list(sys.stdin.readline().strip())
+ORD = lambda: [ord(char) for char in sys.stdin.readline().strip()]
+
+
+from collections import Counter, defaultdict, deque
+from bisect import bisect_right, bisect_left
+from math import ceil, sqrt, gcd
+from heapq import heapify, heappop, heapreplace, heappush
+
+
+
+
+def solve():
+
+    n, k = ILT()
+
+    if n == k:
+        print(0)
+    else:
+        print (max(((n-(k+1))*3 +1 + ((k-1)//2)*3 + ((k-1)%2)),0))
+for _ in range(I()):
+    solve()
