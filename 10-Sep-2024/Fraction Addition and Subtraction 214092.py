45f0e1fb6e2277634c5dc4d7db08a469a128d6c8
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-09-10 17:03:49 +0300

    Created 'Fraction Addition and Subtraction' by A2SV Hub

---
+++ 10-Sep-2024/Fraction Addition and Subtraction 214092.py
@@ -0,0 +1,43 @@
+# Problem: Fraction Addition and Subtraction - https://leetcode.com/problems/fraction-addition-and-subtraction/
+
+class Solution:
+    def fractionAddition(self, expression: str) -> str:
+        result = []; current = ""
+        
+        for i, char in enumerate(expression):
+            if char == "+": result.append(current);current = ""
+            elif char == '-' and i != 0:
+                result.append(current)
+                current = ""
+                
+                current += '-'
+            else:
+                current += char
+
+        result.append(current)  
+
+
+        _lcm = 1
+        denominator = []
+        numerator = []
+        for elem in result:
+            num,denom = map(int,elem.split('/'))
+            _lcm = lcm(denom,_lcm)
+            denominator.append(denom)
+            numerator.append(num)
+            
+        
+        ansnum = 0
+        for i in range(len(numerator)):
+            chung = _lcm//denominator[i]
+
+            ansnum += (chung*numerator[i])
+        
+        comdiv = gcd(ansnum,_lcm)
+
+        ansnum//=comdiv
+        _lcm//=comdiv
+
+        return f"{ansnum}/{_lcm}"
+
+      
