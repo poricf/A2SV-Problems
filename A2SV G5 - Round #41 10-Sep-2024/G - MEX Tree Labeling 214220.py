e9d481d1004f92ac34f3821527f62ec54e54ddcb
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-09-11 00:38:55 +0300

    Created 'G - MEX Tree Labeling' by A2SV Hub

---
+++ A2SV G5 - Round #41 10-Sep-2024/G - MEX Tree Labeling 214220.py
@@ -0,0 +1,33 @@
+# Problem: G - MEX Tree Labeling - https://codeforces.com/gym/544853/problem/E
+
+
+
+
+
+
+
+def solve():
+    n = int(input())
+    d = [0] * (n + 1)
+    edges = [[0, 0] for _ in range(n - 1)]
+    for i in range(n - 1):
+        u, v = map(int, input().split())
+        edges[i][0] = u
+        edges[i][1] = v
+        d[u] += 1
+        d[v] += 1
+    # print(d)
+    left = 0
+    right = n - 2
+
+    for x, y in edges:
+        if d[x] == 1 or d[y] == 1:
+            print(left)
+            left += 1
+        else:
+            print(right)
+            right -= 1
+
+
+# for _ in range(I()):
+solve()
