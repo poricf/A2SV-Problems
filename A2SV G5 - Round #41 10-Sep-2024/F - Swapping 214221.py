c4dd98f481e559814d19dd50d317d924e89b7217
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-09-11 00:39:08 +0300

    Created 'F - Swapping' by A2SV Hub

---
+++ A2SV G5 - Round #41 10-Sep-2024/F - Swapping 214221.py
@@ -0,0 +1,12 @@
+# Problem: F - Swapping - https://codeforces.com/gym/544853/problem/D
+
+
+for _ in range(int(input())):
+	n,x,m= list(map(int,input().split()))
+	a,b=x,x
+	for i in range(m):
+		l , r = list(map(int,input().split()))
+		if b >= l and a <= r:
+			a = min(l,a)
+			b = max(b,r)
+	print(b-a+1)
