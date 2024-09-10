a339fd170c93b3e91859ddc7c3df8a117e07aa2d
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-09-10 15:12:02 +0300

    Created 'Floor and Mod' by A2SV Hub

---
+++ 10-Sep-2024/Floor and Mod 214044.py
@@ -0,0 +1,39 @@
+# Problem: Floor and Mod - https://codeforces.com/problemset/problem/1485/C
+
+
+import sys
+sys.setrecursionlimit(10**3)
+INF = float('inf')
+N = 10**5 + 10
+alpha = '/abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ/' 
+
+def I(): return int(sys.stdin.readline().strip())
+def ILT(): return list(map(int, sys.stdin.readline().strip().split()))
+def S(): return sys.stdin.readline().strip()
+def SLT(): return sys.stdin.readline().strip().split()
+def DIG(): return [int(i) for i in (list(sys.stdin.readline().strip()))]
+def CHAR(): return list(sys.stdin.readline().strip())
+ORD = lambda: [ord(char) for char in sys.stdin.readline().strip()]
+
+
+from collections import Counter, defaultdict, deque
+from bisect import bisect_right, bisect_left
+from math import ceil, sqrt, gcd
+from heapq import heapify, heappop, heapreplace, heappush
+
+
+        
+        
+
+
+def solve():
+    a,b = ILT()
+    c = 0
+    n = 1
+    while n**2<a:
+        c+=max(0,min(b,a//n-1)-n)
+        n+=1
+    print(c)
+
+for _ in range(I()):
+    solve()
