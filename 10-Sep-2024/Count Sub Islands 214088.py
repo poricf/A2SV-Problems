160c0ff79fe8bfc7823fa2fc932daa34df149265
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-09-10 17:00:48 +0300

    Created 'Count Sub Islands' by A2SV Hub

---
+++ 10-Sep-2024/Count Sub Islands 214088.py
@@ -0,0 +1,25 @@
+# Problem: Count Sub Islands - https://leetcode.com/problems/count-sub-islands/
+
+class Solution:
+    def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
+        def dfs(i, j):
+            if i < 0 or i >= m or j < 0 or j >= n or grid2[i][j] == 0:
+                return True
+            grid2[i][j] = 0
+            is_sub_island = grid1[i][j] == 1
+            is_sub_island &= dfs(i - 1, j)
+            is_sub_island &= dfs(i + 1, j)
+            is_sub_island &= dfs(i, j - 1)
+            is_sub_island &= dfs(i, j + 1)
+            return is_sub_island
+        
+        m, n = len(grid1), len(grid2[0])
+        cnt = 0
+        
+        for i in range(m):
+            for j in range(n):
+                if grid2[i][j] == 1:  
+                    if dfs(i, j):
+                        cnt += 1
+                        
+        return cnt
