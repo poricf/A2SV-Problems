b24c2a8860101e55552136e8c91c2a07b4966c9d
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-09-10 17:03:25 +0300

    Created 'N-ary Tree Postorder Traversal' by A2SV Hub

---
+++ 10-Sep-2024/N-ary Tree Postorder Traversal 214091.py
@@ -0,0 +1,25 @@
+# Problem: N-ary Tree Postorder Traversal - https://leetcode.com/problems/n-ary-tree-postorder-traversal/
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        ans = []
+        def dfs(node):
+            if not node:
+                return 
+            for child in node.children:
+                dfs(child)
+            
+            ans.append(node.val)
+        
+        dfs(root)
+
+        return ans
+        
