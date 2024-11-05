789ec9534ecd9c8f9b9872a102ac337823bdb770
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-11-06 00:02:09 +0300

    Created 'Greatest Common Divisor of Strings' by A2SV Hub

---
+++ 05-Nov-2024/Greatest Common Divisor of Strings 223286.py
@@ -0,0 +1,10 @@
+# Problem: Greatest Common Divisor of Strings - https://leetcode.com/problems/greatest-common-divisor-of-strings/
+
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1 + str2 != str2 + str1:
+            return ""
+        n = len(str1)
+        m = len(str2)
+        G = gcd(n , m)
+        return str1[:G]
