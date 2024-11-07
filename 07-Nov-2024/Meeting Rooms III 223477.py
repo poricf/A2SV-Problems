05012790e1e1e197bc9dfd847adc2a93bcd607d4
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2024-11-07 13:50:11 +0300

    Created 'Meeting Rooms III' by A2SV Hub

---
+++ 07-Nov-2024/Meeting Rooms III 223477.py
@@ -0,0 +1,34 @@
+# Problem: Meeting Rooms III - https://leetcode.com/problems/meeting-rooms-iii/
+
+class Solution:
+    def mostBooked(self, n, meetings):
+        ans = [0] * n
+        times = [0] * n
+        meetings.sort()
+
+        for meeting in meetings:
+            start, end = meeting
+            flag = False
+            minind = -1
+            val = float('inf')
+            for j in range(n):
+                if times[j] < val:
+                    val = times[j]
+                    minind = j
+                if times[j] <= start:
+                    flag = True
+                    ans[j] += 1
+                    times[j] = end
+                    break
+            if not flag:
+                ans[minind] += 1
+                times[minind] += (end - start)
+
+        maxi = -1
+        id = -1
+        for i in range(n):
+            if ans[i] > maxi:
+                maxi = ans[i]
+                id = i
+
+        return id
